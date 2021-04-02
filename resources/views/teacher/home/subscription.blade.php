3d8a26989d035c61b7a3e155d1f691c2508a6aef
unknown <malikshafiq7088@gmail.com>
2021-04-02 05:07:42 -0700

    subscription payment added

--- resources/views/teacher/home/subscription.blade.php
+++ resources/views/teacher/home/subscription.blade.php
@@ -40,6 +40,7 @@
                                     <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                                     <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
                                 </form>
+
                             </div>
                         </div>
                     </div>
@@ -60,7 +61,6 @@
                                     <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                                     <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
                                 </form>
-
 
                             </div>
                         </div>
