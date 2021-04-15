f4b76b70a38a011675f8531cc4e4c38103c020fe
unknown <malikshafiq7088@gmail.com>
2021-04-15 03:49:49 -0700

    all new changes done

--- resources/views/teacher/auth/register.blade.php
+++ resources/views/teacher/auth/register.blade.php
@@ -222,10 +222,14 @@
                                                     <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
                                                         <input type="hidden" name="cmd" value="_s-xclick">
                                                         <input type="hidden" name="hosted_button_id" value="EJGPP2YQR4T2N">
-                                                        <input type="hidden" name="item_number" value="1">
-                                                        <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                                        <table>
+                                                            <tr><td><input type="hidden" name="on0" value="cm">cm</td></tr><tr><td><input type="text" name="os0" maxlength="200" value="12"></td></tr>
+                                                            <tr><td><input type="hidden" name="on1" value="item_number">item_number</td></tr><tr><td><input type="text" name="os1" maxlength="200" value="13"></td></tr>
+                                                        </table>
+                                                        <input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribe_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                                                         <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
                                                     </form>
+
                                                 </div>
                                             </div>
                                             <div class="col-sm-5">
